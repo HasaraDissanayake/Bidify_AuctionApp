8e39e33a32a1836cadeddbbae128321583270e41
HasaraDissanayake <dmhasara.d@gmail.com>
2025-05-06 02:19:41 +0800

    to resolve merge conflicts

--- BidifyAuctionApp/Bidify/BidifyTests/BidifyTests.swift
+++ BidifyAuctionApp/Bidify/BidifyTests/BidifyTests.swift
@@ -7,9 +7,18 @@
 
 import Testing
 
+import DeveloperToolsSupport
+import Foundation
+import SwiftOnoneSupport
+import SwiftUI
+import UIKit
+import _Concurrency
+import _StringProcessing
+import _SwiftConcurrencyShims
 
+//@testable import BidEase
 struct BidifyTests {
-
+    
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
